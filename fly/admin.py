7f365c93b63908ff8e63b185ca733ec10d4751e9
stepbystep <likerberi97@gmail.com>
2019-05-28 17:39:06 +0900

    adminClass added and load static

--- fly/admin.py
+++ fly/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Airport, Fly, Passenger
 
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flys.through 
+    extra = 1
+
+class FlyAdmin(admin.ModelAdmin):
+    #list_display = ('',)
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    #list_display = ('',)
+    #django admin, -> & <- delete & add property
+    filter_horizontal = ("flys",)
+
+
 admin.site.register(Airport)
-admin.site.register(Fly)
-admin.site.register(Passenger)
+admin.site.register(Fly, FlyAdmin)
+admin.site.register(Passenger, PassengerAdmin)
 
 # Register your models here.
